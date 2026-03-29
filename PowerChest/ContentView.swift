76426f863e870a0615c49ccfa251641445e4ba9e
Scrips Weave <scrips.weave_0f@icloud.com>
2026-03-29 17:16:49 +0200

    Initial Commit

---
+++ PowerChest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PowerChest
+//
+//  Created by Jan van Rensburg on 3/29/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PowerChest/PowerChestApp.swift
@@ -0,0 +1,17 @@
+//
+//  PowerChestApp.swift
+//  PowerChest
+//
+//  Created by Jan van Rensburg on 3/29/26.
+//
+
+import SwiftUI
+
+@main
+struct PowerChestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
